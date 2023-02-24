fa0c8d3da1d2aac28b5b12b6660b5f96eea94174
Fabio Ishizu <fabio.masao@tecdet.com.br>
2023-02-24 10:13:53 -0300

    [dev] Model usuários e pagina de login

---
+++ ProjetosFrontEnd/Models/Users.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjetosFrontEnd.Models;
+
+public class Users
+{
+    [Key]
+    [Required]
+    public int Id { get; set; }
+    [Required(ErrorMessage = "O UserName precisa ser preenchido.")]
+    [MaxLength(100, ErrorMessage = "O UserName é muito longo.")]
+    public string UserName { get; set; }
+    [Required(ErrorMessage = "A senha precisa ser preenchida.")]
+    [MinLength(8, ErrorMessage = "A senha é muita curta, mínimo de 8 caracteres.")]
+    public string Password { get; set; }
+    public int CollaboratorsId { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+    public DateTime? DeletedAt { get; set; }
+}
